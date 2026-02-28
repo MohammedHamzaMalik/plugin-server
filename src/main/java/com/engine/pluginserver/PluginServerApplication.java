df607c966e87654b798a87967f11a5db97d0de09
Mohammed Hamza Malik <m.h.m.i.malik@gmail.com>
2026-02-27 23:47:50 -0600

    Initial commit

---
+++ src/main/java/com/engine/pluginserver/PluginServerApplication.java
@@ -0,0 +1,13 @@
+package com.engine.pluginserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PluginServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/engine/pluginserver/PluginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.pluginserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluginServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
